19895731173728a36a898ae637d34b7c7b5f847e
jinseanlu <117480992+jinseanlu@users.noreply.github.com>
2023-09-16 11:56:39 -0400

    Create sketch.js

---
+++ person_walking_on_grass/sketch.js
@@ -0,0 +1,26 @@
+function setup() {
+  createCanvas(500, 500);
+  console.log("I'm sub-20 in cubing")
+  print("HEElo hit select is op")
+}
+
+function draw() {
+  var c = color(255, 255, 0)
+  var d = color(255, 229, 180)
+  var g = color(0, 169, 0)
+  var b = color(150, 75, 0)
+  background(0, 180, 255);
+  fill(d)
+  circle(200, 200, 68)
+  line(200, 234, 200, 300)
+  line(200, 267, 210, 250)
+  line(200, 267, 190, 250)
+  line(200, 300,190, 317)
+  line(200, 300,210, 317)
+  fill(c)
+  circle(69, 69, 69)
+  fill(g)
+  rect(0, 317, 500, 45.75)
+  fill(b)
+  rect(0, 362.75, 500, 137.25)
+}
